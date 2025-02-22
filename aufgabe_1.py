b13d163b6732391f0ff879348498439bb2a65fc4
Brxnni <arne.norvin@gmail.com>
2025-02-22 01:30:42 +0100

    A1 & A3 fast fertig

---
+++ aufgabe_1.py
@@ -0,0 +1,26 @@
+from itertools import permutations
+from collections import Counter
+import matplotlib.pyplot as plt
+
+spaces = list(range(1, 10))
+
+distances = []
+perms = list(permutations(spaces))
+
+for perm in perms:
+	distance = sum([abs(perm[i+1]-perm[i]) for i in range(len(perm)-1)])
+	# distance_ = sum([perm[i+1]-perm[i] for i in range(len(perm)-1)])
+
+	distance += perm[0] + perm[-1]
+	distances.append((perm, distance))
+
+twenties = [ d for d in distances if d[1] == 20 ]
+for t in twenties: print(t)
+
+distance_distribution = Counter([ d[1] for d in distances ])
+print(distance_distribution)
+
+plt.bar(distance_distribution.keys(), distance_distribution.values())
+plt.xlabel("Gesamtstrecke")
+plt.ylabel("# Permutationen")
+plt.show()
